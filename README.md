6a1fc9046816d4b99969471d6a2b4242e104aa34
Chris Mainey <c.mainey@nhs.net>
2022-02-01 22:06:41 +0000

    Published version intended for presentation, with licence and readme

---
+++ README.md
@@ -0,0 +1,9 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# Chief Analyst Interview Presentation
+
+This is my presentation for interview with The Health Economics unit.
+
+Click here to access the slides in you browser:
+<https://chrismainey.github.io/chief_analyst_presentation/chief_analyst_interview.html>
--- random_int.R
+++ random_int.R
@@ -7,7 +7,16 @@ library(lme4)
 gpa <- read_csv("https://raw.githubusercontent.com/m-clark/mixed-models-with-R/master/data/gpa.csv")
 
 gpa_sub <- subset(gpa, student <21)
-gpa_mixed = lmer(gpa ~ occasion + (1 | student), data = gpa_sub)
+gpa_mixed <- lmer(gpa ~ occasion + (1 | student), data = gpa_sub)
+gpa_fixed <- lm(gpa ~ occasion, data = gpa_sub)
+
+coef(gpa_fixed)[[2]]
+
+gpa_sub$rints <- rep(ranef(gpa_mixed)[[1]][[1]],each=6)
+gpa_sub$plot_rints <- coef(gpa_fixed)[[1]] + gpa_sub$rints
+gpa_sub$slp <- coef(gpa_fixed)[[2]]
+
+
 
 theme_set(
   theme_minimal()+
@@ -38,8 +47,10 @@ c <- ggplot(gpa_sub, aes(y=gpa, x=occasion))+
 
 d <- ggplot(gpa_sub, aes(y=gpa, x=occasion))+
   geom_point(aes(col=as.factor(student)), show.legend = FALSE, size=2.5)+
-  stat_smooth(aes(col=as.factor(student)), method="lm", geom='line', alpha=0.5
-              , se=FALSE, size=1.2, show.legend = FALSE)+
+  geom_abline(aes(intercept = plot_rints, slope=slp, col=as.factor(student))
+              , alpha=0.5, size=1.2, show.legend = FALSE)+
+  # stat_smooth(aes(col=as.factor(student)), method="lm", geom='line', alpha=0.5
+  #             , se=FALSE, size=1.2, show.legend = FALSE)+
   geom_smooth(method = "lm", linetype = "dashed", size=1.2)+
   labs(title = "Random-intercept example"
        ,subtite = "subset of 'GPA' dataset")
